fab85a361ee6bf888c2e2ae2dadcbc741d4b0582
malawadd <50057882+malawadd@users.noreply.github.com>
2022-08-18 00:57:47 +0300

    🧑‍💼 added market dahsboard

---
+++ components/Markets.js
@@ -0,0 +1,149 @@
+import s from "../styles/Markets.module.scss";
+import { useState } from "react";
+import Image from "next/image";
+import { gql, useQuery } from "@apollo/client";
+import { useRetrieveTokenData } from "../hooks/useRetrieveTokenData";
+
+const GET_OPEN_POOLS = gql`
+  query ($state: String!, $orderBy: BigInt!, $orderDirection: String!) {
+    poolEntities(orderBy: $orderBy, orderDirection: $orderDirection, where: { state: $state }) {
+      createdAtTimestamp
+      poolId
+      tokenAddress
+      basePrice
+      tresholdPrice
+      feePercentage
+      insuranceLiquidityAdded
+      startDate
+      endDate
+    }
+  }
+`;
+
+export const Markets = ({ account }) => {
+  const [showSupplyModal, setShowSupplyModal] = useState(false);
+  const [showRequestModal, setShowRequestModal] = useState(false);
+  const [modalData, setModalData] = useState();
+  const [modalPoolData, setModalPoolData] = useState();
+
+  // Query the list of created pools from the subgraph
+
+  const {
+    loading: poolsLoading,
+    error: poolsError,
+    data: poolsData,
+  } = useQuery(GET_OPEN_POOLS, {
+    variables: {
+      state: "created",
+      orderBy: "startDate",
+      orderDirection: "asc",
+    },
+  });
+
+  const pools = poolsData?.poolEntities;
+  const validPools = pools?.filter((pool, index) => {
+    const now = new Date();
+    const unixtime = (now.valueOf() / 1000).toFixed(2);
+    console.log(`unixtime: ${unixtime}`);
+    console.log(pool.startDate);
+    if (pool.startDate > unixtime) {
+      return pool;
+    } else {
+      return false;
+    }
+  });
+
+  const retrieveValidityPeriod = (start, end) => {
+    const startDate = new Date(start * 1000).toLocaleDateString("en-GB");
+    const startTime = new Date(start * 1000).toLocaleTimeString("en-GB");
+    const endDate = new Date(end * 1000).toLocaleDateString("en-GB");
+    const endTime = new Date(end * 1000).toLocaleTimeString("en-GB");
+    return `${startDate} (${startTime}) - ${endDate} (${endTime})`;
+  };
+
+  // This is an helper function to get the cover loss percentage.
+  const calculateLossPercentage = (tresholdPrice, basePrice) => {
+    const difference = basePrice - tresholdPrice;
+    return ((difference * 100) / basePrice).toFixed(2);
+  };
+
+  //Other helper function to retrieve the right logo and name based on the address.
+  const retrieveTokenData = (tokenAddress) =>
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useRetrieveTokenData(tokenAddress);
+
+  const handleSupplyModal = (data, pool) => {
+    setModalData(data);
+    setModalPoolData(pool);
+    setShowSupplyModal(true);
+  };
+
+  const handleRequestModal = (data, pool) => {
+    setModalData(data);
+    setModalPoolData(pool);
+    setShowRequestModal(true);
+  };
+
+  return (
+    <div className={s.container}>
+    <h3 className={s.tabHeader}>Open Taamin Pools</h3>
+    <div>
+      <div className={s.tableRow}>
+        <p className={s.assetHead}>Asset</p>
+        <p className={s.priceLossHead}>Price Loss</p>
+        <p className={s.feeHead}>Fee</p>
+        <p className={s.validityDataHead}>Validity Period</p>
+      </div>
+      {poolsLoading && <div>...loading</div>}
+      <div className={s.dataContainer}>
+        {validPools?.map((item, index) => (
+          <div key={index} className={s.tableRow}>
+            <div className={s.assetData}>
+              <Image
+                src={retrieveTokenData(item.tokenAddress).logo}
+                width={30}
+                height={30}
+              />
+            </div>
+            <p className={s.priceLossData}>
+              {`${calculateLossPercentage(
+                item.tresholdPrice,
+                item.basePrice
+              )} % (< $${(item.tresholdPrice / 10 ** 8).toFixed(4)})`}
+            </p>
+            <p className={s.feeData}>{item.feePercentage} %</p>
+            <div className={s.validityDataData}>
+              <p>{retrieveValidityPeriod(item.startDate, item.endDate)}</p>
+            </div>
+
+            <div className={s.dataButtonContainer}>
+              <button
+                onClick={() =>
+                  handleSupplyModal(
+                    retrieveTokenData(item.tokenAddress),
+                    item
+                  )
+                }
+                className={s.dataButton}
+              >
+                Supply
+              </button>
+              <button
+                onClick={() =>
+                  handleRequestModal(
+                    retrieveTokenData(item.tokenAddress),
+                    item
+                  )
+                }
+                className={s.dataButton}
+              >
+                Request
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+    </div>
+  );
+};
--- components/TabSelector.js
+++ components/TabSelector.js
@@ -3,12 +3,15 @@ import { NoWalletDashboard } from "./NoWalletDashboard";
 import { TabButtonContainer } from "./TabButtonContainer";
 import { WalletDashboard } from "./WalletDashboard";
 import { Pool } from "./Pool";
+import { Markets } from "./Markets";
 
 export const TabSelector = ({ walletConnected, account }) => {
   const [selected, setSelected] = useState("Dashboard");
 
   const renderTab = () => {
     switch (selected) {
+      case "Markets":
+        return <Markets account={account} />;
       case "Pool":
         return <Pool account={account} />;
       default:
