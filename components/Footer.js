76a046e14f38b365402e79b71f5974977f7bf622
malawadd <50057882+malawadd@users.noreply.github.com>
2022-08-19 23:48:09 +0300

    🍗 update footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,17 @@ import { FaHeart, FaCoffee } from "react-icons/fa"
 export const Footer = () => {
     return (
         <footer className={s.footer}>
-            my name
+            <p className={s.footerText}>
+            Built for  {"  "}
+            <a
+            target="_blank"
+            rel="noreferrer"
+            className={s.footerLink}
+            href="https://buidlit.polygon.technology/"
+            >
+               Polygon BUIDL IT : Summer 2022 {"  "}
+            </a>
+            </p>
         </footer>
     )
 }
